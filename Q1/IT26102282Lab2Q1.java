b65d795de314ceb928c0b23388d512fc0559813e
IT26102282 <IT26102282@my.sliit.lk>
2026-06-30 14:30:20 +0530

    Add Lab2Q1.java to Q1

---
+++ Q1/IT26102282Lab2Q1.java
@@ -0,0 +1,11 @@
+public class IT26102282Lab2Q1{
+  
+  public static void main(String args[]){
+     double perimeter = 100;
+	 double length,width;
+	 length = perimeter / 3.5;
+	 width = (3*length) /4;
+	   System.out.println("length = " + length);
+	   System.out.println("width = " + width);
+     }
+  }
