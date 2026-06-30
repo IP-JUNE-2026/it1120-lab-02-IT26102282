9482d8305ef950984a4c2d6169061c45b22a5cf2
IT26102282 <IT26102282@my.sliit.lk>
2026-06-30 14:31:25 +0530

    Add Lab2Q2.java to Q2

---
+++ Q2/IT26102282Lab2Q2.java
@@ -0,0 +1,12 @@
+public class IT26102282Lab2Q2{
+  
+  public static void main(String args[]){
+     double side = 10;
+     double perimeter;
+     double radius;
+     double pi=3.14; 
+        perimeter = 4* side; //perimeter of the square
+        radius = perimeter/(2*3.14); //radius of the circle
+	   System.out.println("Radius of the circular fence=" + radius);
+     }
+  }
